67d1ddc8c5f27a5f1edadf1fe2f58eec3cc0a483
Aaron Chiandet <achiandet@gmail.com>
2014-11-02 11:25:38 -0700

    Portfolio preview changes

--- js/main.js
+++ js/main.js
@@ -1,41 +1,9 @@
-$(function () {
-    
-    var filterList = {
-    
-        init: function () {
-        
-            // MixItUp plugin
-            // http://mixitup.io
-            $('#portfoliolist').mixitup({
-                targetSelector: '.portfolio',
-                filterSelector: '.filter',
-                effects: ['fade'],
-                easing: 'snap',
-                // call the hover effect
-                onMixEnd: filterList.hoverEffect()
-            });             
-        
-        },
-        
-        hoverEffect: function () {
-        
-            // Simple parallax effect
-            $('#portfoliolist .portfolio').hover(
-                function () {
-                    $(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
-                    $(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');             
-                },
-                function () {
-                    $(this).find('.label').stop().animate({bottom: -50}, 200, 'easeInQuad');
-                    $(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');                               
-                }       
-            );              
-        
-        }
-
-    };
-    
-    // Run the show!
-    filterList.init();
-    
-});     
+// MixItUp: This handles the filter and sort animations on portfoliio elements
+$(function(){
+    $('#portfoliolist').mixitup({
+        targetSelector: '.portfolio',
+        filterSelector: '.filter',
+        effects: ['fade'],
+        easing: 'snap'
+    });
+});
